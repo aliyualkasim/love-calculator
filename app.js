332de1334b67f748778a5559d996c9d8082900e0
obsessedyouth <52631736+obsessedyouth@users.noreply.github.com>
2020-03-08 15:35:23 +0100

    snippet template logic

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-//Get the names of the two love birds
-let yourName = prompt("What is your name?");
-let partnerName = prompt("Whats your partners name?");
+//Get the names of the two love birds", the submit button and display message
+
+/* This can be refactored to reduce DOM manipulation */
+const yourName = document.querySelector("#yourName");
+const partnerName = document.querySelector("#partnerName");
+const message = document.querySelector("#message");
+const button = document.querySelector(".calculator form button");
+
+/* event listener to calcuate love */
+button.addEventListener("click",calculate);
+
+function calculate(){
+    // love calculation logic can go here
+}
